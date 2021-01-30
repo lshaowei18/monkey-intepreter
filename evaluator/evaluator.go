977de7e4474b1388296e64696cb344b7fc24412a
Shao Wei <lshaowei18@gmail.com>
2021-01-30 13:21:54 +0800

    feat: evaluate array index expression

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -64,6 +64,8 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		return evalCallExpression(node, env)
 	case *ast.ArrayLiteral:
 		return evalArrayLiteral(node, env)
+	case *ast.IndexExpression:
+		return evalIndexExpression(node, env)
 
 	default:
 		fmt.Printf("type: %T, value: %v\n", node, node)
@@ -291,6 +293,36 @@ func evalCallExpression(ce *ast.CallExpression, env *object.Environment) object.
 	return applyFunction(function, args)
 }
 
+func evalIndexExpression(ie *ast.IndexExpression, env *object.Environment) object.Object {
+	left := Eval(ie.Left, env)
+	if isError(left) {
+		return left
+	}
+
+	index := Eval(ie.Index, env)
+	if isError(index) {
+		return index
+	}
+
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+	arrayObject := array.(*object.Array)
+	idx := index.(*object.Integer).Value
+	max := int64(len(arrayObject.Elements) - 1)
+
+	if idx < 0 || idx > max {
+		return NULL
+	}
+
+	return arrayObject.Elements[idx]
+}
 func applyFunction(fn object.Object, args []object.Object) object.Object {
 	switch fn := fn.(type) {
 	case *object.Function:
