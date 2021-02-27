3ff39f840717186ec2f4f0d8cabd3a97454ff5c3
Shao Wei <lshaowei18@gmail.com>
2021-02-27 13:07:04 +0800

    refactor: introduce executeBinaryOperation

--- vm/vm.go
+++ vm/vm.go
@@ -45,13 +45,8 @@ func (vm *VM) Run() error {
 				return err
 			}
 			ip += 2
-		case code.OpAdd:
-			err := vm.handleAdd()
-			if err != nil {
-				return err
-			}
-		case code.OpSub:
-			err := vm.handleSub()
+		case code.OpAdd, code.OpSub:
+			err := vm.executeBinaryOperation(op)
 			if err != nil {
 				return err
 			}
@@ -62,34 +57,42 @@ func (vm *VM) Run() error {
 	return nil
 }
 
+func (vm *VM) executeBinaryOperation(op code.Opcode) error {
+	right := vm.pop()
+	left := vm.pop()
+
+	leftType := left.Type()
+	rightType := right.Type()
+
+	if leftType == object.INTEGER_OBJ && rightType == object.INTEGER_OBJ {
+		return vm.executeBinaryIntegerOperation(op, left, right)
+	}
+
+	return fmt.Errorf("unsupported types for binary operation: %s %s", leftType, rightType)
+}
+
+func (vm *VM) executeBinaryIntegerOperation(op code.Opcode, left, right object.Object) error {
+	leftValue := left.(*object.Integer).Value
+	rightValue := right.(*object.Integer).Value
+
+	var result int64
+
+	switch op {
+	case code.OpAdd:
+		result = leftValue + rightValue
+	case code.OpSub:
+		result = leftValue - rightValue
+	default:
+		return fmt.Errorf("unknown integer operator: %d", op)
+	}
+
+	return vm.push(&object.Integer{Value: result})
+}
+
 func (vm *VM) handleConstant(ip int) error {
 	constIndex := code.ReadUint16(vm.instructions[ip+1:])
 
 	err := vm.push(vm.constants[constIndex])
-	return err
-}
-
-func (vm *VM) handleAdd() error {
-	right := vm.pop()
-	left := vm.pop()
-
-	leftValue := left.(*object.Integer).Value
-	rightValue := right.(*object.Integer).Value
-
-	result := leftValue + rightValue
-	err := vm.push(&object.Integer{Value: result})
-	return err
-}
-
-func (vm *VM) handleSub() error {
-	right := vm.pop()
-	left := vm.pop()
-
-	leftValue := left.(*object.Integer).Value
-	rightValue := right.(*object.Integer).Value
-
-	result := leftValue - rightValue
-	err := vm.push(&object.Integer{Value: result})
 	return err
 }
 
